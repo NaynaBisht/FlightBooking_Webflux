e929a7537c2c4b98a157f0aa0b28184834666135
NaynaBisht <nayna.22bce8037@vitapstudent.ac.in>
2025-11-23 16:29:45 +0530

    Search by pnr

--- flight-booking-app/src/main/java/com/flightapp/controller/BookingController.java
+++ flight-booking-app/src/main/java/com/flightapp/controller/BookingController.java
@@ -27,10 +27,22 @@ public class BookingController {
 
 	@GetMapping("/ticket/{pnr}")
 	public Mono<ResponseEntity<Booking>> getTicketDetailsByPnr(@PathVariable String pnr) {
-		log.info("Fetching ticket details for PNR={}", pnr);
+	    log.info("Fetching ticket details for PNR={}", pnr);
 
-		return bookingService.getBookingByPnr(pnr).map(response -> ResponseEntity.ok(response));
+	    return bookingService.getBookingByPnr(pnr)
+	            .map(ResponseEntity::ok)
+	            .onErrorResume(ex -> {
+	                log.error("Error fetching ticket for PNR {}: {}", pnr, ex.getMessage());
+	                String msg = ex.getMessage() == null ? "" : ex.getMessage();
+
+	                if (msg.contains("not found")) {
+	                    return Mono.just(ResponseEntity.notFound().build()); // 404
+	                }
+
+	                return Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build());
+	            });
 	}
+
 
 	@GetMapping("/booking/history/{emailId}")
 	public Mono<ResponseEntity<List<Booking>>> getBookingHistory(@PathVariable String emailId) {
